43fd164db11e1cb41c542597520692432e05851f
tuyisenge250 <benjaminwell250@gmail.com>
2023-09-18 13:49:26 -0700

    ./6-puts2

--- 0x05-pointers_arrays_strings/main.h
+++ 0x05-pointers_arrays_strings/main.h
@@ -9,5 +9,11 @@ void reset_to_98(int *n);
 int _strlen(char *s);
 void _puts(char *str);
 int _atoi(char *s);
+void _puts(char *str);
+void print_rev(char *s);
+void rev_string(char *s);
+void puts2(char *str);
+void puts_half(char *str);
+void print_array(int *a, int n);
 
 #endif

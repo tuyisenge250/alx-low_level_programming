b71f1c51d3084e166484422680f06cf31e30bcfd
tuyisenge250 <benjaminwell250@gmail.com>
2023-09-21 12:02:45 -0700

    char *cap_string(char *);

--- 0x06-pointers_arrays_strings/6-cap_string.c
+++ 0x06-pointers_arrays_strings/6-cap_string.c
@@ -6,12 +6,9 @@
  *
  * Return: the resulting string;
  */
-char *leet(char *s)
+char *cap_string(char *)
 {
 	int i, j;
-
-	char *a = "aAeEoOtTlL";
-	char *b = "4433007711";
 
 	for (i = 0; s[i] != '\0'; i++)
 	{
